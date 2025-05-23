63ae3132fbe1d480115e8b5e02c5c0b3873767b9
Hugo W Sanchez <hwsanchez.poreda@gmail.com>
2025-05-23 14:24:29 +0200

    Last commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 //Note: The array has to have an even number of elements in order to work!
-//This new comment was added in a different branch
 public class Main {
     public static void main(String[] args) {
         int[] num1 = {1,2,3,4,5,6,7,8,9,10, 11, 12, 13, 14};
