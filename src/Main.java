4eb50e5ea82b61224de945a4543217deb90b68ab
Hugo W Sanchez <hwsanchez.poreda@gmail.com>
2025-05-23 13:45:46 +0200

    Initial commit with Java source code only

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] num1 = {1,2,3,4,5,6,7,8,9,10, 11, 12, 13, 14};
+        int lenNum1 = num1.length;
+
+        int[] shuffled = new int[lenNum1];
+        for (int i = 0; i < lenNum1 - 1; i++) {
+            if(i * 2 + 1 <= lenNum1){
+                shuffled[2 * i] = num1[i];
+                shuffled[2 * i + 1] = num1[lenNum1/2 + i];
+            }
+
+        }
+        System.out.println("Original array: " + Arrays.toString(num1));
+        System.out.println("Shuffled array:" + Arrays.toString(shuffled));
+    }
+}
